ea430319531f7b83a1daf69e8f9aeda4640be363
Pawel Pekrol <ppekrol@ravendb.net>
2019-10-15 14:42:53 +0200

    RavenDB-14126 added ArrayPool and ObjectPool to ByteTool

---
+++ SharpSnmpLib/Pools.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.ObjectPool;
+using System;
+using System.Collections.Generic;
+
+namespace Lextm.SharpSnmpLib
+{
+    public static class Pools
+    {
+        private static readonly ObjectPool<List<byte>> ByteListPool = new DefaultObjectPool<List<byte>>(new DefaultPooledObjectPolicy<List<byte>>());
+
+        public static List<byte> GetByteList()
+        {
+            return ByteListPool.Get();
+        }
+
+        public static void ReturnByteList(List<byte> list)
+        {
+            if (list == null)
+                throw new ArgumentNullException(nameof(list));
+
+            list.Clear();
+
+            ByteListPool.Return(list);
+        }
+    }
+}
